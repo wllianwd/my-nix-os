972f3c81550bdf9447869b1cf792e2a024269f22
wllianwd <willian.wd@gmail.com>
2021-11-24 09:29:37 +0100

    chore: added vim plugins

--- users/willian/home.nix
+++ users/willian/home.nix
@@ -8,7 +8,6 @@
 
   # imports
   imports = [ 
-    #./nvim/nvim.nix
     ./dconf/dconf.nix
   ];
   
@@ -41,11 +40,20 @@
     };
     vim = {
       enable = true;
-      plugins = with pkgs.vimPlugins; [ vim-airline nerdtree nerdtree-git-plugin ];
+      plugins = with pkgs.vimPlugins; [ 
+        vim-airline 
+        nerdtree 
+        nerdtree-git-plugin 
+        vim-devicons 
+        vim-nerdtree-syntax-highlight 
+      ];
       settings = { ignorecase = true; };
       extraConfig = ''
         set mouse=a
+        autocmd StdinReadPre * let s:std_in=1
+        autocmd VimEnter * NERDTree | if argc() > 0 || exists("s:std_in") | wincmd p | endif
+        autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
       '';
     };
   };
-}
+};
