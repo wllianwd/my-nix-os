139498a13d4c2896a8ee181590bf252fce15e2cb
Willian Dallastella <willian.wd@gmail.com>
2024-07-25 23:33:30 +0200

    Update system

--- system/configuration.nix
+++ system/configuration.nix
@@ -84,8 +84,8 @@ in {
         "vm.max_map_count" = 524288;
       };
     };
-    kernelPackages = pkgs.linuxPackages_testing;
-    #kernelPackages = pkgs.linuxPackages_latest;
+    #kernelPackages = pkgs.linuxPackages_testing;
+    kernelPackages = pkgs.linuxPackages_latest;
     #kernelPackages = pkgs.linuxPackages;
     #kernelParams = [ "acpi_enforce_resources=lax" ];
   };
